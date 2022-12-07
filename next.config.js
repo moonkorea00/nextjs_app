edccc60c706233a2556a35d12b70e84a0e79f7d5
moonkorea00 <moonkorea00@gmail.com>
2022-12-07 11:36:35 +0900

    getServerSideProps

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/Hey/:path*',
+        destination: '/Main',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/custom',
+        destination: `https://jsonplaceholder.typicode.com/${process.env.NEXT_PUBLIC_API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
